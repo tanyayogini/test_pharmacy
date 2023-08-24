cb39a0cd355f069bb440d89310db4200a6a20cc9
tatianakushnareva <tvkushnareva@yandex.ru>
2023-08-24 13:56:57 +0500

    fix empty distribution note

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db.models import Count
+
 from products.models import Product, ProductClient, Distribution
 
 
@@ -13,6 +15,11 @@ class ProductClientAdmin(admin.ModelAdmin):
 class DistributionAdmin(admin.ModelAdmin):
     list_display = ('product', 'names', 'count_names')
     read_only_fields = ('product', 'names', 'count_names')
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(total=Count('product__names')).filter(total__gt=0)
+        return queryset
 
 
 admin.site.register(Product, ProductAdmin)
